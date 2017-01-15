924f1bb385481674786a16995765dba1480c7bdc
Manh Dao Van <manhdaovan@gmail.com>
2017-01-15 11:38:03 +0900

    Add test case for buil query string of Index class with non-index field

--- test/test_index.rb
+++ test/test_index.rb
@@ -6,9 +6,14 @@ class TestIndex < Test::Unit::TestCase
   def test_query_string
     types = %w(VARCHAR TEXT BLOB INT DATE OTHERS)
     # Index for primary
+    # 1. without any non-index field
     index = MysqlWarmup::Index.new('table_name', 'col_name', 'INT', 'PRI')
-    assert_equal(build_query('sum(`table_name`.`col_name`)', '`table_name`', '1'),
+    assert_equal(build_query('count(*)', '`table_name`', '1'),
                  index.query_string)
+    # 2. with non-index field
+    index2 = MysqlWarmup::Index.new('table_name', 'col_name', 'INT', 'PRI', 'non_index')
+    assert_equal(build_query('count(*)', '`table_name`', '`table_name`.`non_index` = 0'),
+                 index2.query_string)
 
     # Index for other fields
     types.each do |type|
