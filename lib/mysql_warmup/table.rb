a44a928e6caa9c9a5dad2f80d77a0804f2c306d4
Manh Dao Van <manhdaovan@gmail.com>
2017-01-15 11:37:19 +0900

    Change build query string in Index class with non-index field

--- lib/mysql_warmup/table.rb
+++ lib/mysql_warmup/table.rb
@@ -20,13 +20,17 @@ module MysqlWarmup
     private
 
     def build_index
-      indexes_infos = @field_infos.select { |v| !v[DESC_TABLE_STRUCTURE[:key]].empty? }
-      indexes       = []
+      indexes_infos    = []
+      non_indexes_info = []
+      @field_infos.each { |v| v[DESC_TABLE_STRUCTURE[:key]].empty? ? non_indexes_info << v : indexes_infos << v }
+      non_index_field = non_indexes_info.empty? ? nil : non_indexes_info[0][DESC_TABLE_STRUCTURE[:field]]
+      indexes         = []
       indexes_infos.each do |index_info|
         indexes << MysqlWarmup::Index.new(@table_name,
                                           index_info[DESC_TABLE_STRUCTURE[:field]],
                                           index_info[DESC_TABLE_STRUCTURE[:type]],
-                                          index_info[DESC_TABLE_STRUCTURE[:key]])
+                                          index_info[DESC_TABLE_STRUCTURE[:key]],
+                                          non_index_field)
       end
       indexes
     end
